3abe0ddde5e323db4b580a3f1af13214fdb782c1
Alex Delgado <gregoriougarte8@gmail.com>
2023-03-03 23:57:25 -0400

    class1: Dinosaur
    Lo que hicimos fue hacer que nos aparezca la imagen del dinosaurio haciendolo correr, ademas le unapusimos la funcion  pra que pueda saltar y por ultimo hicimos variables para que el codigo se vea mas limpio

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,5 +1,7 @@
 import pygame
 
+from dino_runner.components.dinosaur import Dinosaur
+
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
@@ -14,6 +16,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,7 +34,8 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update()
 
     def draw(self):
         self.clock.tick(FPS)
@@ -38,6 +43,7 @@ class Game:
         self.draw_background()
         pygame.display.update()
         pygame.display.flip()
+        self.player.draw(self.screen)
 
     def draw_background(self):
         image_width = BG.get_width()
